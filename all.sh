6be4e0560cb10211e61d2b93dce745c46a41a1b4
Felix Dörre <felix@dogcraft.de>
2015-05-02 14:20:13 +0200

    Adding crl generation to all.sh

--- all.sh
+++ all.sh
@@ -4,9 +4,14 @@
 
 echo "========== Generating Root ======="
 . ./generateKeys.sh
+
 echo "========== Generating Year 2015 ======="
 . ./generateTime.sh 2015
 echo "========== Generating Infra for Year 2015 ======="
 . ./generateInfra.sh 2015
+echo "========== Generating CRLs for Year 2015 ======="
+. ./generateCRLs.sh 2015
+
+
 echo "========== Verifying Year 2015 ======="
 . ./verify.sh 2015
